7cc83be99b179cef427c8ad25c3c9f109ad39a96
Cory Loken <cory@me.com>
2024-07-31 14:30:00 -0400

    Initial v2 conversion

--- test-app/tests/integration/components/hero-icon-test.js
+++ test-app/tests/integration/components/hero-icon-test.js
@@ -1,5 +1,5 @@
 import { render } from '@ember/test-helpers';
-import { setupRenderingTest } from 'dummy/tests/helpers';
+import { setupRenderingTest } from 'test-app/tests/helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
 
